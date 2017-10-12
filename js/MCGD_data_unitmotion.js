f3d283a1379b58edab7c8e793669f1005518cfdc
Gary <garynil1635@gmail.com>
2017-10-12 21:35:02 +0800

    Add unitmotion data

--- js/MCGD_data_unitmotion.js
+++ js/MCGD_data_unitmotion.js
@@ -1,6 +1,14 @@
 CsvToHtmlTable.init({
-	csv_path: 'data/unitmotion/udata.csv', 
-	element: 'table-container', 
+	csv_path: 'data/unitmotion/data_unitmotion_count.csv', 
+	element: 'table-container-count', 
+	allow_download: false,
+	csv_options: {separator: ',', delimiter: '"'},
+	datatables_options: {"paging": false,"aaSorting": [[1, 'asc']]}
+});
+
+CsvToHtmlTable.init({
+	csv_path: 'data/unitmotion/data_unitmotion_time.csv', 
+	element: 'table-container-time', 
 	allow_download: false,
 	csv_options: {separator: ',', delimiter: '"'},
 	datatables_options: {"paging": false}
