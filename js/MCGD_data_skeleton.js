3809910b6ed83fb3c03548c2a0c32ebbbb0bb55a
Gary <garynil1635@gmail.com>
2017-10-12 16:40:27 +0800

    Add agreement

--- js/MCGD_data_skeleton.js
+++ js/MCGD_data_skeleton.js
@@ -1,58 +1,20 @@
 
-$.getJSON("./data/skeleton/skeleton-data-files.json", function(data) {
-// 	console.log(data["Files"]);
-	var files_array = data["Files"];
-	listAllFiles(files_array);
-});
-
-function listAllFiles(file_arr) {
-    for (var index in file_arr) {
-// 		console.log(file_arr[index]);
-		processFileNameToListName(file_arr[index]);
-    }
-}
-
-// process json file name to html format
-function processFileNameToListName(filename){
-// 	console.log(filename);
-	
-	var arr1 = filename.split('.');
-	var arr2 = arr1[0].split(' #');
-	
-	title = arr2[0];
-	var gunarr = arr2[0].split('_');
-// 	console.log(gunarr);
-	var groupID = gunarr[0];
-	var group = "Group："+gunarr[0].replace("G", "");
-// 	console.log(group);
-	var user = "User："+gunarr[1];
-	console.log(user);
-	var mode = "Mode："+gunarr[2].replace("M", "")[0];
-// 	console.log(mode);
-	var time = "Recored on "+arr2[1].split('T')[0];
-// 	console.log(time);
-	filename = filename.replace(" ", "%20");
-	filename = filename.replace("#", "%23");
-	addFilesToHTML(filename,filename,title,group,groupID,mode,user,time);
-}
-
-
 // add name to html
 function addFilesToHTML(filename,fullname,titlecnt,group,groupID,mode,user,time){
-	console.log($('#sk-data-list'));
+// 	console.log($('#sk-data-list'));
 	
 	var divhead = "<div class=\"card-footer text-muted\">";
 	
 	// Title
 	var titleHead = "<hh4 class=\"sk-data-dl-h2\">";
 	var titleEnd = "</hh4>";
-	var title = titleHead + titlecnt + titleEnd
+	var title = titleHead + titlecnt + titleEnd;
 // 	G1_U2_MA2A
 	
 	// content
 	// <p>Group：1&emsp;&emsp;Mode：Audio&emsp;&emsp;User：U2&emsp;&emsp;Recored on 2015_08_16</p>
 	var cnthead = "<p>";
-	cnt = group + "&emsp;&emsp;" + mode + "&emsp;&emsp;" + user + "&emsp;&emsp;" + time;
+	var cnt = group + "&emsp;&emsp;" + mode + "&emsp;&emsp;" + user + "&emsp;&emsp;" + time;
 	var cntend = "</p>";
 	cnt = cnthead + cnt + cntend;
 	
@@ -64,14 +26,57 @@ function addFilesToHTML(filename,fullname,titlecnt,group,groupID,mode,user,time)
 	var link = "./data/skeleton/"+groupID+'/'+fullname;
 	var linkhead2 = " \" class=\"btn btn-outline-primary sk-data-dl-btn\">";
 	
-	var linkend = "Download &darr;</a>";
+	var linkend = "View <i class=\"fa fa-eye\" aria-hidden=\"true\"></i></a>";
 	var linkcnt = linkhead1 + link + linkhead2 + linkend ; 
 	//end
-	var divend = "</div>"
+	var divend = "</div>";
 	
-	this_html = divhead+title+cnt+linkcnt+divend;
+	var this_html = divhead+title+cnt+linkcnt+divend;
 	$('#sk-data-list').append(this_html);
 }
+
+
+
+// process json file name to html format
+function processFileNameToListName(filename){
+// 	console.log(filename);
+	
+	var arr1 = filename.split('.');
+	var arr2 = arr1[0].split(' #');
+	
+	var title = arr2[0];
+	var gunarr = arr2[0].split('_');
+// 	console.log(gunarr);
+	var groupID = gunarr[0];
+	var group = "Group："+gunarr[0].replace("G", "");
+// 	console.log(group);
+	var user = "User："+gunarr[1];
+// 	console.log(user);
+	var mode = "Mode："+gunarr[2].replace("M", "")[0];
+// 	console.log(mode);
+	var time = "Recored on "+arr2[1].split('T')[0];
+// 	console.log(time);
+	filename = filename.replace(" ", "%20");
+	filename = filename.replace("#", "%23");
+	addFilesToHTML(filename,filename,title,group,groupID,mode,user,time);
+}
+
+function listAllFiles(file_arr) {
+    for (var index in file_arr) {
+// 		console.log(file_arr[index]);
+		processFileNameToListName(file_arr[index]);
+    }
+}
+
+$.getJSON("./data/skeleton/skeleton-data-files.json", function(data) {
+// 	console.log(data["Files"]);
+	var files_array = data.Files;
+	listAllFiles(files_array);
+});
+
+
+
+
 
 /*
 $( document ).ready(function() {
